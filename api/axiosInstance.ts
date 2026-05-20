dda4b1472cae3a984468a1cf3851fe7e0cb86a72
Hadiur <fmhadiuzzaman@gmail.com>
2026-05-21 01:08:09 +0200

    configure EAS production workflow

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,26 +1,24 @@
-
 // src/api/axiosInstance.ts
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://api.mrbrbackpunkte.de/api',
+  baseURL: "http://35.158.164.156:3010/api",
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 axiosInstance.interceptors.request.use(
   async (config) => {
-    const token = await AsyncStorage.getItem('userToken');
+    const token = await AsyncStorage.getItem("userToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
-
